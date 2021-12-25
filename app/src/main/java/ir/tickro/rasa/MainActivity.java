144946a8be71905e44efdacc79a393ed97b58a3a
hamed sadeghi <hamed.sadeghi190@gmail.com>
2021-12-25 15:41:23 +0330

    fix errors

--- app/src/main/java/ir/tickro/rasa/MainActivity.java
+++ app/src/main/java/ir/tickro/rasa/MainActivity.java
@@ -2,6 +2,7 @@ package ir.tickro.rasa;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.annotation.SuppressLint;
@@ -17,45 +18,80 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.Manifest;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import pub.devrel.easypermissions.helper.PermissionHelper;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class MainActivity extends  AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Fragment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
     WebView rasaWebView;
+    public static final int REQUEST_SELECT_FILE = 100;
+    public ValueCallback<Uri[]> uploadMessage;
+    private ValueCallback<Uri> mUploadMessage;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private String mCM;
+    private ValueCallback<Uri> mUM;
+    private ValueCallback<Uri[]> mUMA;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        loadSite1();
+        loadSite();
     }
 
 
     @AfterPermissionGranted(100)
     private void loadSite() {
-        String[] perms = {Manifest.permission.CAMERA,Manifest.permission.INTERNET,Manifest.permission.ACCESS_FINE_LOCATION};
-        if (EasyPermissions.hasPermissions(this, perms)) {
+
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+        boolean granted = EasyPermissions.hasPermissions(this, perms);
+
+        if (granted) {
             loadSite1();
         } else {
-            EasyPermissions.requestPermissions(this, "We need permissions because this and that",100, perms);
+            EasyPermissions.requestPermissions(this, "We need permissions because this and that", 100, perms);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
@@ -71,8 +107,6 @@ public class MainActivity extends  AppCompatActivity implements EasyPermissions.
         }
     }
 
-
-
     public void loadSite1() {
         rasaWebView = findViewById(R.id.rasaWebView);
         WebSettings settings = rasaWebView.getSettings();
@@ -81,18 +115,124 @@ public class MainActivity extends  AppCompatActivity implements EasyPermissions.
         settings.setLoadsImagesAutomatically(true);
         settings.setAllowContentAccess(true);
         settings.setAppCacheEnabled(true);
-        settings.setDomStorageEnabled(true);
         settings.setLoadWithOverviewMode(true);
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
         settings.setPluginState(WebSettings.PluginState.ON);
         settings.setMediaPlaybackRequiresUserGesture(false);
 
-        /* CUSTOM USER AGENT */
-//        settings.setUserAgentString("hcapp");
+        settings.setAllowFileAccess(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        settings.setAllowFileAccess(true);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            settings.setMixedContentMode(0);
+            rasaWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            rasaWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT < 19) {
+            rasaWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
         rasaWebView.setWebViewClient(new MyWebViewClient());
         rasaWebView.loadUrl("https://rasa.tickro.ir");
+        rasaWebView.setWebChromeClient(new WebChromeClient()
+        {
+            // For 3.0+ Devices (Start)
+            // onActivityResult attached before constructor
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    intent = fileChooserParams.createIntent();
+                }
+                try
+                {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch ( android.content.ActivityNotFoundException e)
+                {
+                    uploadMessage = null;
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+            {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+        });
+
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (requestCode == REQUEST_SELECT_FILE)
+            {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        }
+        else if (requestCode == FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+
+
+    }
+
+    // Create an image file
+    private File createImageFile() throws IOException {
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
 
     public void onBackPressed() {
         if (rasaWebView.canGoBack()) {
@@ -103,6 +243,11 @@ public class MainActivity extends  AppCompatActivity implements EasyPermissions.
     }
 
     private class MyWebViewClient extends WebViewClient {
+
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             rasaWebView.loadUrl(url);
@@ -114,5 +259,27 @@ public class MainActivity extends  AppCompatActivity implements EasyPermissions.
             handler.proceed();
         }
 
+        // reference to activity instance. May be unnecessary if your web chrome client is member class.
+        private MainActivity myActivity;
+
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+            // make sure there is no existing message
+            if (myActivity.uploadMessage != null) {
+                myActivity.uploadMessage.onReceiveValue(null);
+                myActivity.uploadMessage = null;
+            }
+
+            myActivity.uploadMessage = filePathCallback;
+
+            Intent intent = fileChooserParams.createIntent();
+            try {
+                myActivity.startActivityForResult(intent, MainActivity.REQUEST_SELECT_FILE);
+            } catch (android.content.ActivityNotFoundException e) {
+                myActivity.uploadMessage = null;
+                Toast.makeText(myActivity, "Cannot open file chooser", Toast.LENGTH_LONG).show();
+                return false;
+            }
+            return true;
+        }
     }
 }
